61cedfbd09893f04dfe1393dee5964f0ec30a7d7
Khan <huang.khan74@gmail.com>
2015-11-24 13:25:36 -0500

    new stuff

--- express_server.js
+++ express_server.js
@@ -4,5 +4,10 @@ var app = express('');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname))
 
-app.listen(3000);
-console.log("Server running on port 3000");
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
